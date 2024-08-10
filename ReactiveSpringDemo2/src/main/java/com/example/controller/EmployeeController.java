c07814c67f1d748ccbecef4144046c2d8a7b31ce
hiteshnathya <hiteshnathya@gmail.com>
2024-08-10 16:39:14 +0530

    UPDATE

--- ReactiveSpringDemo2/src/main/java/com/example/controller/EmployeeController.java
+++ ReactiveSpringDemo2/src/main/java/com/example/controller/EmployeeController.java
@@ -4,9 +4,11 @@ import com.example.model.Employee;
 import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.*;
-
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 public class EmployeeController<e> {
@@ -19,4 +21,25 @@ public class EmployeeController<e> {
     }
 
 
-}
+    @RequestMapping(value = {"/id"}, method = RequestMethod.GET)
+    public ResponseEntity<Mono<Employee>> findById(@PathVariable("id")Integer id)
+    {
+        Mono<Employee> e= employeeService.findById(id);
+        return new ResponseEntity<Mono<Employee>>(e,HttpStatus.FOUND);
+    }
+
+
+    @RequestMapping(value = {"/name{/name}"}, method = RequestMethod.GET)
+    Flux<Employee>findByName(String name){
+
+        return employeeService.findByName(name);
+    }
+
+
+    Flux<Employee>findAll(){
+         Flux<Employee> emps=employeeService.findAll();
+         return emps;
+    }
+
+    }
+
